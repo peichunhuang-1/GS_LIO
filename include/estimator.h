7e474b47887df13ed0125c94abd4d32965faecfb
peichunhuang <p8l2n2s2g2k@gmail.com>
2026-03-14 19:38:04 +0800

    feat: estimator class

---
+++ include/estimator.h
@@ -0,0 +1,29 @@
+#ifndef GS_LIO_ESTIMATOR_H
+#define GS_LIO_ESTIMATOR_H
+
+#include "state.h"
+#include <deque>
+
+namespace gs_lio
+{
+
+class estimator
+{
+public:
+  estimator() = default;
+  virtual ~estimator() = default;
+  virtual void zupt(const stamp_t &tailstamp);
+  virtual bool forward(const stamp_t &tailstamp = -1) {return false;}
+  virtual void optimize() {}
+  virtual void reset(const state_t &state);
+protected:
+  static std::shared_mutex state_mtx;
+  static state_t optimal_state;
+  state_t get_state() const;
+  void set_state(const state_t &state);
+  std::deque<state_t> propagated_queue;
+};
+
+}
+
+#endif
---
+++ src/estimator.cc
@@ -0,0 +1,34 @@
+#include "estimator.h"
+
+namespace gs_lio
+{
+
+std::shared_mutex estimator::state_mtx;
+
+state_t estimator::optimal_state;
+
+void estimator::zupt(const stamp_t &tailstamp)
+{
+  // TODO: implement zero-velocity update
+}
+
+void estimator::reset(const state_t &state)
+{
+  std::unique_lock lock(estimator::state_mtx);
+  estimator::optimal_state = state;
+  propagated_queue.clear();
+}
+
+state_t estimator::get_state() const
+{
+  std::shared_lock lock(estimator::state_mtx);
+  return estimator::optimal_state;
+}
+
+void estimator::set_state(const state_t &state)
+{
+  std::unique_lock lock(estimator::state_mtx);
+  estimator::optimal_state = state;
+}
+
+}
