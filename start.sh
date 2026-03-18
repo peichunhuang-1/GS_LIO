1d779aae033fbe97c7dff56d1ea188f7c4a66227
peichunhuang-1 <p8l2n2s2g2k@gmail.com>
2026-03-18 13:05:16 +0000

    feat: change start.sh image name

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v $(pwd):/ros2_ws/src/gs-lio --mount type=bind,source=/dev/shm,target=/dev/shm --network host --shm-size=1g -e DISPLAY=:10.0 -v /tmp/.X11-unix:/tmp/.X11-unix --ipc=host gs-lio 
+docker run --rm -it -v $(pwd):/ros2_ws/src/gs-lio --mount type=bind,source=/dev/shm,target=/dev/shm --network host --shm-size=8g -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --ipc=host gs_lio 
